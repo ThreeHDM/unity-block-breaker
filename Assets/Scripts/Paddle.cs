06978b375c97d83eb4c0926842422aec7c536d61
ThreeHDM <juanbilardi@gmail.com>
2020-06-06 14:51:46 -0300

    53. Move Object With Mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    //Los numeros de tipo float deben tener una f
+    [SerializeField] float ScreenWidthUnits = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Buscamos la posición en cada eje (x,y) por frame. Calculamos la proporcion con el ancho de la pantalla. Hay 16 unidades de ancho por lo que multiplicamos por ese número.
+        //Debug.Log(Input.mousePosition.x / Screen.width * ScreenWidthUnits);
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * ScreenWidthUnits;
+        //Creamos un vector2 (2D) para guardar las coordenadas.
+        // esto es type varname = new Object
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        //Seteamos la posición del GameObject refiriendonos al componente transform y a la propiedad position
+        transform.position = paddlePos;
+
+    }
+}
