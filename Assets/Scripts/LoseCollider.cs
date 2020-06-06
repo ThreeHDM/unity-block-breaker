19a4607d751e754571120f30ddf610de1b49537c
ThreeHDM <juanbilardi@gmail.com>
2020-06-06 12:46:23 -0300

    52. Colliders, Collision & Triggers - adding collider and setting a trigger event - coding the LoseCollider Script that takes you to the Game Over Scene using a string (not recommended)

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    //Programamos el comportamiento que dispara el trigger. Cuando algo pasa por el LoseCollider
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+}
