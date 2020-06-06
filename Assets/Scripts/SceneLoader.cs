f7fa6f75abee233a7984819414fa71f1e5641f39
ThreeHDM <juanbilardi@gmail.com>
2020-06-06 11:23:58 -0300

    Importing Scenes and Scripts from Number Wizard.  - deleted and re arraged several game objects - renamed scenes

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//agregamos el namespace SceneManagement para usar todo su contenido
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        //declaramos una variable que trainga el índice actual de la escena usando SceneManager class
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        //Cargamos la siguiente escena a la que estamos
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
