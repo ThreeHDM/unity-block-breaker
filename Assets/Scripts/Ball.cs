e6cd54f25816d6d440f102123db68b3d610d8c9b
ThreeHDM <juanbilardi@gmail.com>
2020-06-13 12:37:08 -0300

    56. Launch The Ball - Capturando el input del mouse para disparar la ball a una determinada velocidad

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,9 +8,12 @@ public class Ball : MonoBehaviour
 
     //Configuration fields
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
 
     //State
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
 
     // Start is called before the first frame update
@@ -21,6 +25,27 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+        }
+        
+        LaunchOnMouseClick();
+
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            //Excepto para el componente transform, debemos usar GetComponent para manipular otros componentes. Como por ej, rigidbody
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         //Creamos un vector que nos dice la posición actual del paddle
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
