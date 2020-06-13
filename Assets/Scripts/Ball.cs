e32377d85f5b989f5b8fc2a73ed221eebc5bd1bb
ThreeHDM <juanbilardi@gmail.com>
2020-06-13 12:01:05 -0300

    55. Distance As A Vector2 - sticking the ball to the paddle calculating the distance between vectors

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    //Configuration fields
+    [SerializeField] Paddle paddle1;
+
+    //State
+    Vector2 paddleToBallVector;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Calculamos la diferencia entre los vectores ( ball y paddle)
+        paddleToBallVector = transform.position - paddle1.transform.position; // si no aclaro nada transform se refiere al Gameobject del script. En este caso Ball
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Creamos un vector que nos dice la posición actual del paddle
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+        
+    }
+}
