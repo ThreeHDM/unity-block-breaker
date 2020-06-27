699047012a09798647c796194b05e05ebb704dff
ThreeHDM <juanbilardi@gmail.com>
2020-06-27 11:58:18 -0300

    58. Destroy Game Object

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    //Creo el método que se dispara cuando hay una colisión. Lo dispara el Engine
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Destruimos el game object. Podríamos destruir un asset o un component también. El segundo parametro es el tiempo (se le pasa un float)
+        Destroy(gameObject);
+    }
+}
