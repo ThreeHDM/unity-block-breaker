34c98f73e0cf57af8cf96841a696aa1c450a22e0
ThreeHDM <juanbilardi@gmail.com>
2020-07-11 11:42:01 -0300

    66. Win The Level & Load the next Scene

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-
+    //Parameters
     //We create a variable to keep track of the amount of blocks. We SerializeField so we can see it in the inspector for debugging purposes
     [SerializeField] int breakableBlocks;
+
+    //cached reference
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
 
     //We create a public method to count the breakable blocks. Each time is called we add 1 to the total number of breakable blocks
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
+    }
+
+    //We decrease the number of blocks each time one is broken
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
     }
 }
