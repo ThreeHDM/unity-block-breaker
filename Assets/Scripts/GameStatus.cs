f0bd47c39f1d1bbb903bfe8e02f471b5861e82fa
ThreeHDM <juanbilardi@gmail.com>
2020-07-18 12:05:45 -0300

    69. Display Player Score - using TextMeshProUGUI

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Llamamos a TMPro
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
@@ -8,10 +10,17 @@ public class GameStatus : MonoBehaviour
     //Creo un campo en la UI de Unity con un valor predeterminado para la velocidad del juego. Además le agrego [Range] lo que transforma a la UI en un slider entre el min y el max
     [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
+    //Creamos un campo serializado para scoreText usando TextMeshProUGUI. En ese campo vinculamos el ScoreText GameObject
+    [SerializeField] TextMeshProUGUI scoreText;
 
     //State variables
     [SerializeField] int currentScore = 0; // inicializamos el score en 0
-    
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -21,5 +30,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 }
